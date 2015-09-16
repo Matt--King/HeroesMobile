e681754b608fed32f40175c83daef725b5657628
Aakash Shah <ahs2hd@virginia.edu>
2015-09-16 01:32:08 -0400

    GPS - still not working :( #3

--- app/src/main/java/edu/virginia/cs/cs4720/heroesmobile/Home.java
+++ app/src/main/java/edu/virginia/cs/cs4720/heroesmobile/Home.java
@@ -54,95 +54,90 @@ public class Home extends AppCompatActivity {
     private LocationListener locationListener;
     private Location loc;
     public void showGPS(View v) {
-        Log.i("GPS","Show GPS Started");
+        Log.i("GPS", "Show GPS Started");
         locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // TODO check for GPS enabled
-        try {
-            loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        } catch (SecurityException e) {
+        if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             Toast.makeText(getBaseContext(),
-                    "You didn't let me use your location.",
+                    "Location is not enabled. Location cannot be found.",
                     Toast.LENGTH_LONG).show();
-        }
-        // Log.i("GPS","before loc == null check");
-        loc = null;
-        // TODO delete above line
-        boolean l = (loc == null);
-        Log.i("GPS","loc == null evaluates to "+String.valueOf(l));
-        if (loc == null) {
-            // Log.i("GPS","LKN == null");
-            // Def  ine a listener that responds to location updates
-            locationListener = new LocationListener() {
-                private final String TAG = "debug.LocationListener";
+        } else {
+            loc = null;
+            // TODO remove above line
+            if (loc == null) {
+                // Log.i("GPS","LKN == null");
+                // Def  ine a listener that responds to location updates
+                locationListener = new LocationListener() {
+                    private final String TAG = "debug.LocationListener";
 
-                public void onLocationChanged(Location location) {
-                    Log.i(TAG, "onLocationChanged executed");
-                    loc = location;
-                    Log.v(TAG, "IN ON LOCATION CHANGE, lat=" + loc.getLatitude() + ", lon=" + loc.getLongitude());
-                }
-
-                // assume default behavior on these 3 methods
-                public void onProviderEnabled(String p) {
-                    Log.i(TAG, "Provider enabled");
-                }
-
-                public void onProviderDisabled(String p) {
-                    Log.i(TAG, "Provider disabled");
-                }
-
-                public void onStatusChanged(String p, int status, Bundle extras) {
-                    Log.i(TAG, "Status changed");
-                }
-            };
-
-            try {
-                // Log.i("GPS","loc == null");
-                GregorianCalendar cal = new GregorianCalendar();
-                Log.i("GPS", "starting timer");
-
-                while (loc == null){
-                    locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                }
-                //loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                Log.i("GPS","loc == null evaluates to "+String.valueOf(l));
-                long start = cal.getTimeInMillis();
-                // Log.i("GPS", "Location "+loc.toString());
-                while (loc.getAccuracy() > 68) {
-
-                    if (cal.getTimeInMillis() - start > 5000){
-                        Toast.makeText(getBaseContext(),
-                                "Location not found.",
-                                Toast.LENGTH_LONG).show();
-                        break;
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "onLocationChanged executed");
+                        loc = location;
+                        Log.v(TAG, "IN ON LOCATION CHANGE, lat=" + loc.getLatitude() + ", lon=" + loc.getLongitude());
                     }
+
+                    // assume default behavior on these 3 methods
+                    public void onProviderEnabled(String p) {
+                        Log.i(TAG, "Provider enabled");
+                    }
+
+                    public void onProviderDisabled(String p) {
+                        Log.i(TAG, "Provider disabled");
+                    }
+
+                    public void onStatusChanged(String p, int status, Bundle extras) {
+                        Log.i(TAG, "Status changed");
+                    }
+                };
+
+                try {
+                    // Log.i("GPS","loc == null");
+                    GregorianCalendar cal = new GregorianCalendar();
+                    Log.i("GPS", "starting timer");
+
+                    while (loc == null) {
+                        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    }
+                    //loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    Log.i("GPS", "loc == null evaluates to " + String.valueOf(l));
+                    long start = cal.getTimeInMillis();
+                    // Log.i("GPS", "Location "+loc.toString());
+                    while (loc.getAccuracy() > 68) {
+
+                        if (cal.getTimeInMillis() - start > 5000) {
+                            Toast.makeText(getBaseContext(),
+                                    "Location not found.",
+                                    Toast.LENGTH_LONG).show();
+                            break;
+                        }
+                    }
+                    locManager.removeUpdates(locationListener);
+                    Log.i("GPS", "listener turned off");
+
+
+                } catch (SecurityException e) {
+                    Toast.makeText(getBaseContext(),
+                            "You didn't let me use your location.",
+                            Toast.LENGTH_LONG).show();
                 }
-                locManager.removeUpdates(locationListener);
-                Log.i("GPS", "listener turned off");
-
-
-            } catch (SecurityException e) {
-                Toast.makeText(getBaseContext(),
-                        "You didn't let me use your location.",
-                        Toast.LENGTH_LONG).show();
             }
+            Log.i("GPS", "loc found");
+            //Edit TextViews Latitude and Longitude to output the lat and long
+            double lat = loc.getLatitude();
+            double lon = loc.getLongitude();
+
+            TextView latBox = (TextView) findViewById(R.id.latitude);
+            latBox.setText("Your Latitude: " + lat);
+            TextView lonBox = (TextView) findViewById(R.id.longitude);
+            lonBox.setText("Your Longitude: " + lon);
+
+            //when the GPS button is pressed, make it disappear
+            (findViewById(R.id.button2)).setVisibility(View.GONE);
+            (findViewById(R.id.textView2)).setVisibility(View.GONE);
+
+            (findViewById(R.id.textView4)).setVisibility(View.VISIBLE);
+            findViewById(R.id.latitude).setVisibility(View.VISIBLE);
+            findViewById(R.id.longitude).setVisibility(View.VISIBLE);
         }
-        Log.i("GPS","loc found");
-        //Edit TextViews Latitude and Longitude to output the lat and long
-        double lat = loc.getLatitude();
-        double lon = loc.getLongitude();
-
-        TextView latBox = (TextView) findViewById(R.id.latitude);
-        latBox.setText("Your Latitude: " + lat);
-        TextView lonBox = (TextView) findViewById(R.id.longitude);
-        lonBox.setText("Your Longitude: " + lon);
-
-        //when the GPS button is pressed, make it disappear
-        (findViewById(R.id.button2)).setVisibility(View.GONE);
-        (findViewById(R.id.textView2)).setVisibility(View.GONE);
-
-        (findViewById(R.id.textView4)).setVisibility(View.VISIBLE);
-        findViewById(R.id.latitude).setVisibility(View.VISIBLE);
-        findViewById(R.id.longitude).setVisibility(View.VISIBLE);
     }
         @Override
         protected void onCreate (Bundle savedInstanceState){
