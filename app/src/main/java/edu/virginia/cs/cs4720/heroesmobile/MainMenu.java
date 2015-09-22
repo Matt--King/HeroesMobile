57b123cc20bb8b1c15211008f790a4106232ebe3
Matt--King <kingmatt333@hotmail.com>
2015-09-22 16:38:22 -0400

    Created a Main Menu Activity to hold links to other features Previous menu activity renamed to "GPS Screen"

--- app/src/main/java/edu/virginia/cs/cs4720/heroesmobile/MainMenu.java
+++ app/src/main/java/edu/virginia/cs/cs4720/heroesmobile/MainMenu.java
@@ -1,36 +1,29 @@
 package edu.virginia.cs.cs4720.heroesmobile;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+public class MainMenu extends AppCompatActivity {
 
-public class Splash extends AppCompatActivity {
-
+    public void goToGPSScreen(View view)
+    {
+        Intent intent = new Intent(MainMenu.this, GPSScreen.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        //Display the splash page for 1.5 secs, then kill it and start the app
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent intent = new Intent(Splash.this, MainMenu.class);
-                startActivity(intent);
-                Splash.this.finish();
-            }
-        }, 1500);
-
+        setContentView(R.layout.activity_main_menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
     }
 
